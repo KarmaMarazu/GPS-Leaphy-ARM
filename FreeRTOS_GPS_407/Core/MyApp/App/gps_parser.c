9af2020dcb0f6b7dc93c90c5d6925976b6bbff7f
Marazu <98053409+KarmaMarazu@users.noreply.github.com>
2025-10-02 14:31:27 +0200

    Update gps_parser.c
    remove debug crash

--- FreeRTOS_GPS_407/Core/MyApp/App/gps_parser.c
+++ FreeRTOS_GPS_407/Core/MyApp/App/gps_parser.c
@@ -36,26 +36,6 @@ void GNRMC_Parser(void)
 	GNRMC_data.speed = atof(gnrmc.speed);
 	GNRMC_data.course = atof(gnrmc.course);
 
-	if (Uart_debug_out & GPS_DEBUG_OUT)
-	 	{
-			char lat[sizeof(GNRMC_data.latitude)];
-	 		snprintf(lat,sizeof(GNRMC_data.latitude)+1, "%f", GNRMC_data.latitude);
-
-	 		char lon[sizeof(GNRMC_data.longitude)];
-	 		snprintf(lon, sizeof(GNRMC_data.longitude)+1, "%f", GNRMC_data.longitude);
-
-	 		char spe[sizeof(GNRMC_data.speed)];
-	 		snprintf(spe, sizeof(GNRMC_data.speed)+1, "%f", GNRMC_data.speed);
-
-	 		char cou[sizeof(GNRMC_data.course)];
-	 		snprintf(cou, sizeof(GNRMC_data.course)+1, "%f", GNRMC_data.course);
-
-			UART_puts("\r\n\t latitude double:\t\t"); UART_puts(lat);
-			UART_puts("\r\n\t longitude double:\t");  UART_puts(lon);
-			UART_puts("\r\n\t speed double:    \t");  UART_puts(spe);
-			UART_puts("\r\n\t course double:   \t");  UART_puts(cou);
-	 	}
-
  	xSemaphoreGive(hGNRMC_Struct_Sem); // geef de mutex weer vrij voor een ander
 }
 
