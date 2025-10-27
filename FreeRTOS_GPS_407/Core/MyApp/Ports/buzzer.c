45dbb23256473db22f95efaefc33bafc9eaaa5a0
Marazu <98053409+KarmaMarazu@users.noreply.github.com>
2025-10-27 10:50:33 +0100

    Refactor comments and documentation in buzzer.c
    Updated documentation format and removed redundant comments.

--- FreeRTOS_GPS_407/Core/MyApp/Ports/buzzer.c
+++ FreeRTOS_GPS_407/Core/MyApp/Ports/buzzer.c
@@ -1,16 +1,10 @@
-/*
-Author: 	J.F. van der Bent
-			W Pielage & E Helmond
-
-Date:		24-11-2021
-Revision:	5
-
-    buzzer.c:
-          HAL_ Buzzer-driver for ARM-board v5
-
-    pin-info:
-           PC8 - Buzzer
-
+/**
+* @file buzzer.c
+* @brief Buzzer driver for ARM-board v5
+* @details pin-info: PC8 - Buzzer
+* @author J.F. van der Bent
+* @version 5
+* @date 24-11-2021
 */
 
 #include "main.h"
