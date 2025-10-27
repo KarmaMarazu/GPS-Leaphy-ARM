0f8b497b3f6548378a78eff4e7985d9b01fdaa46
Marazu <98053409+KarmaMarazu@users.noreply.github.com>
2025-10-27 10:13:49 +0100

    Delete FreeRTOS_GPS_407 directory
    delete

--- FreeRTOS_GPS_407/Doxygen/html/annotated_dup.js
+++
@@ -1,4 +0,0 @@
-var annotated_dup =
-[
-    [ "TaskData", "struct_task_data.html", "struct_task_data" ]
-];
--- FreeRTOS_GPS_407/Doxygen/html/student_8c.js
+++
@@ -1,4 +0,0 @@
-var student_8c =
-[
-    [ "Student_task1", "student_8c.html#a282e21c3f1a0eb3054bbb9e40ffa9cc6", null ]
-];
