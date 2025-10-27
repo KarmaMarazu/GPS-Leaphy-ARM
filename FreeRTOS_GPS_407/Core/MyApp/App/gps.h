a217779c5f201e6ab8e2ef35cc58191bf0ac6f72
Marazu <98053409+KarmaMarazu@users.noreply.github.com>
2025-10-27 11:19:51 +0100

    Update gps.h

--- FreeRTOS_GPS_407/Core/MyApp/App/gps.h
+++ FreeRTOS_GPS_407/Core/MyApp/App/gps.h
@@ -1,120 +1,161 @@
 /**
-* @file my_gps.h
-* @brief Bevat basic defines & externals voor de gps routines
-* @attention
-* <h3>&copy; Copyright (c) 2023 (HU)
-* @author MSC, Twan Ton, Mika Dahlkamp, Jasper Verduin en Thomas van Ooijen
+* @file gps_parser.c
+* @brief Veranderd de data die binnen komt in gps.c zodat dit bruikbare cijfers worden. <BR>
+* Bevat functie die het opslaan van waypoints regelt.
+* De waypoint die wordt opgeslagen is het gemiddelde van de 3 laatst opgeslagen gps punten.
+* @author Twan Ton, Mika Dahlkamp, Thomas van Ooijen en Jasper Verduin
 *
-* @date 5/9/2023
+* @date 1/10/2025.
 */
 
-// DEFINES start: ==================================================================================================
-// bepalen hoeveel logs er genomen worden.
-#define MAX_LOGS 100
+#include <admin.h>
+#include "main.h"
+#include "cmsis_os.h"
+#include "gps.h"
 
-// enum voor NMEA protocolstrings (starting 'e' for enum)
-enum NMEA
+
+
+GNRMC gnrmc; 						  // global struct for GNRMC-messages
+Data_Parser GNRMC_data;
+Data_Parser Gem;					  // struct om huidige positie in op te slaan maar dan als gemiddelde van 3 punten
+Data_Parser average[3];				  // struct om een gemiddelde van de opgeslagen data te nemen als waypoint.
+Data_Parser waypoints[MAX_WAYPOINTS]; // struct waar in nuttige data in bruikbare cijfers wordt gezet.
+struct Log DataLog;
+struct Log LogArray[MAX_LOGS];    	  // Array om logs op te slaan
+int logIndex = 0;                     // Huidige index in de array
+
+
+/**
+* @brief functie om gemiddelde van 3 datapunten op de slaan voor nauwkeurigere locatie.
+* @param int i
+* @return void
+*/
+void Average_Bepalen_Waypoints(int i)
 {
-	eGNRMC = 1,
-	eGPGSA,
-	eGNGGA
-};
-// DEFINES stop: ==================================================================================================
+	waypoints[i].latitude = (average[0].latitude + average[1].latitude + average[2].latitude)/3; 	//gemiddelde wordt berekend en opgeslagen
+	waypoints[i].longitude = (average[0].longitude + average[1].longitude + average[2].longitude)/3;
+}
 
-
-// FUNCTIES start: ==================================================================================================
-int hex2int(char *c);
-int hexchar2int(char c);
-int checksum_valid(char *string);
-// FUNCTIES stop: ==================================================================================================
-
-
-//STRUCTURES start: ====================================================================================================
-/// GNRMC struct: all with char-members - should/could be improved with proper data-elements
-typedef struct _GNRMC
+/**
+* @brief functie om de LogArray[] te resetten.
+* @param void
+* @return void
+*/
+void ResetLogArray(void)
 {
-	char    head[7];       // 0. header
-	char    time[11];      // 1. hhmmss.sss
-	char    status;        // 2. A=valid, V=not valid
-	char    latitude[13];  // 3. ddmm.mmmm (double)
-	char    NS_ind;        // 4. N,S
-	char    longitude[13]; // 5. ddmm.mmmm (double)
-	char    EW_ind;        // 6. E,W
-	char    speed[9];      // 7. 0.13 knots (double)
-	char    course[9];     // 8. 309.62 degrees (double)
-	char    date[7];       // 9. ddmmyy
-	char    mag_var[9];    // 10.E,W degrees (double)
-	char    mag_var_pos;   // 11.
-	char    mode;          // 12.A=autonomous, D,E
-	char    cs[4];         // 13.checkum *34
-} GNRMC;
+    xSemaphoreTake(hLog_Struct_Sem, portMAX_DELAY);
+    memset(LogArray, 0, sizeof(LogArray));
+    logIndex = 0;
+    xSemaphoreGive(hLog_Struct_Sem);
+}
 
-/// Data_Parser struct: slaat alleen de benodigde data op in correcte datatypen
-typedef struct _Data_Parser
+
+/**
+* @brief De chars van de ingevulde GNRMC struct moeten omgezet worden naar rekenbare cijfers
+* om van chars naar doubles te gaan gebruik je atof().<BR>
+* Vervolgens wordt deze data in een Data_Parser struct gestopt.
+* Deze struct wordt door een teller in een nieuwe array gezet om hiervan het gemiddelde uit te rekenen.
+* @return void
+*/
+void GNRMC_Parser(void* argument)
 {
-	char 	status;
-	double  latitude;  		// 3. ddmm.mmmm (double)
-	double  longitude; 		// 5. ddmm.mmmm (double)
-	double  course;   	// 8. 309.62 degrees (double)
-} Data_Parser;
+	int i = 0; 												// teller voor schrijven naar average array voor nauwkeurigere waypoint opslaan
+	char val1[10];
+	char val2[10];
+
+	while(TRUE)
+	{
+		xTaskNotifyWait(0,0, NULL, portMAX_DELAY);			// wacht op notify van fill_GNRMC in gps.c
+
+		xSemaphoreTake(hGNRMC_Struct_Sem, portMAX_DELAY); 	// wacht op toegang tot de mutex;
+
+		if(!(gnrmc.status == 'A'))							// status check
+		{
+			GNRMC_data.status = 'V';
+			xSemaphoreGive(hGNRMC_Struct_Sem);
+			continue;
+		}
+
+		memset(&GNRMC_data, 0, sizeof(Data_Parser)); 		// clear the struct
+		memset(val1, 0, sizeof(val1));						// val1 en val2 clearen
+		memset(val2, 0, sizeof(val2));
+
+		memcpy(val1, gnrmc.latitude, 2);					// val1 en val2 vullen val1 krijgt eerste 2 karakters en val2 krijgt 3 t/m 8
+		memcpy(val2, gnrmc.latitude+2, 8);
+		GNRMC_data.latitude = (atof(val1))+(atof(val2)/60);	// atof conversie bij val1 en val2, val2 nog delen door 60 om tot juiste conversie te komen
+		memset(val1, 0, sizeof(val1));						// val1 en val2 clearen
+		memset(val2, 0, sizeof(val2));
+		memcpy(val1, gnrmc.longitude, 3);					// val1 en val2 vullen val1 krijgt eerste 3 karakters en val2 krijgt 4 t/m 8
+		memcpy(val2, gnrmc.longitude+3, 8);
+		GNRMC_data.longitude = (atof(val1))+(atof(val2)/60);// atof conversie bij val1 en val2, val2 nog delen door 60 om tot juiste conversie te komen
+		GNRMC_data.course = atof(gnrmc.course);
+		GNRMC_data.status = 'A';							// Valid status meegeven voor later gebruik
+
+		average[i] = GNRMC_data;							// zet data round robin in average[].
+
+		xSemaphoreGive(hGNRMC_Struct_Sem); 					// geef de mutex weer vrij voor een ander
+
+		xSemaphoreTake(hLog_Struct_Sem, portMAX_DELAY);		// pak de mutex zodat halve data word geschreven als een log 'punt'
+		DataLog.Route = GNRMC_data;
+		xSemaphoreGive(hLog_Struct_Sem);					// geef mutex terug zodat de sturct kan worden opgslagen log 'punt'
 
 
+		i++;
+		if(i >= 3)											// als average[] vol is, dan opnieuw beginnen met vullen
+			i = 0;
+	}
+}
 
-/// Struct voor het bij elkaar houden van de vector tussen Leaphy en Waypoint
-typedef struct Vector
+/**
+* @brief het gemiddelde van wat er in average[] staat wordt opgeslagen met een knop op het ARM-bordje voor meer nauwkeurigheid.<BR>
+* Ook kan deze opgeslagen waypoints gerest kunnen worden met een andere knop. Deze knop krijgt de task door van ARM_keys_IRQ
+* @return void
+*/
+void data_opslaanTask(void *argument)
 {
-	double lengte;
-	double course;
-} Vector;
+	uint32_t key;
+	int i = 0;
+	while(TRUE)
+	{
+		xTaskNotifyWait (0x00, 0xffffffff, &key, portMAX_DELAY);		// wacht op ARM_keys_IRQ
 
+		if(key == 0x0002) 												// als resetknop is ingedrukt dan moet de arrat geleegd worden en de teller moet op 0
+		{
+			memset(&waypoints, 0, sizeof(waypoints));
+			i = 0;
+			LCD_clear(); 												// LCD legen
+			LCD_putint(i); 												// waypoint nummer op LCD
+			LCD_put("/30 waypoints");
+			continue;													// begin boven aan en wacht weer op een arm-key
+		}
 
-/// struct om als verzamelplaats te fungeren voor data die gelogd moeten worden. Deze data wordt op verschillende plaatsen opgeslagen.
-struct Log
-{
-	Data_Parser 	Route;
-	int		TijdSindsStart;
-};
+		if (i<MAX_WAYPOINTS && key == 0x0001)
+		{
+		    xSemaphoreTake(hGNRMC_Struct_Sem, portMAX_DELAY);			// wacht op toegang tot de mutex;
 
-// Struct voor de data log deze kan uitgelezen worden door de debug poort aantesluiten op je laptop
-// en de terminal te openen zodra het juiste knopje is ingedrukt wordt deze struct geprint
-//typedef struct _DataLog
-//{
-//	TickType_t VerstrekenTijd;
-//	char ErrorLog [200][200];
-//	Data_Parser Route [200];
-//	char LeaphyActie[200];
-//} Data_Log;
-//STRUCTURES stop: ====================================================================================================
+		    if(!(GNRMC_data.status == 'A'))								// status check
+		    {
+		    	LCD_clear(); 											// LCD legen
+		    	LCD_putint(i); 											// waypoint nummer op LCD
+		    	LCD_put("/30 waypoints Geen Data");
+		    	xSemaphoreGive(hGNRMC_Struct_Sem);
+		    	continue;
+		    }
+		    Average_Bepalen_Waypoints(i);
+		    xSemaphoreGive(hGNRMC_Struct_Sem); 							// wacht op toegang tot de mutex;
 
+			i++;
 
-
-
-
-// EXTERNS start: ==================================================================================================
-/// struct waar gps string in wordt opgeslagen
-extern GNRMC gnrmc;
-/// struct om de string om te zetten naar cijfers
-extern Data_Parser GNRMC_data;
-/// struct om huidige positie in op te slaan
-extern Data_Parser Gem;
-/// array van structs om de waypoints van de route in op te slaan
-extern Data_Parser waypoints[MAX_WAYPOINTS];
-/// array van structs om hiervan een 1 gemiddelde waarde te maken
-extern Data_Parser average[3];
-/// struct om als verzamelplaats te fungeren voor data die gelogd moeten worden
-extern struct Log DataLog;
-/// array van structs om hierin op te slaan wat er gebeurd terwijl in drive mode
-extern struct Log LogArray[MAX_LOGS];
-
-// functie om de LogArray te resetten staat in gps_parser.c
-extern void ResetLogArray(void);
-
-extern int logIndex;
-// EXTERNS stop: ==================================================================================================
-
-
-
-
-
-//extern int k;
-//extern Data_Log Log;
+		    LCD_clear(); 												// LCD legen
+		    LCD_putint(i); 												// waypoint nummer op LCD
+		    LCD_put("/30 waypoints");
+		    osDelay(100);												// spam beveiliging
+		}
+		else if (i == MAX_WAYPOINTS)									// het maximaal aantal waypoints is bereikt
+		{
+			 LCD_put("Limit reached!");
+			 osDelay(100);												// zonder dit loopt de lcd vast
+			 i++;														// ga hierna uit de if statement zodat niet heel de lcd vol komt te staan
+		}
+	}
+}
